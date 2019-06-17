adaecf63d3cf8f4ccb1ea58d90e563504e514b58
Velten Heyn <github@valoric.de>
2019-06-17 14:47:36 +0200

    Update spec.js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,17 @@
-describe('page', () => {
-  it('works', () => {})
+describe('user > login', () => {
+  beforeEach(() => {
+    cy.visit('https://advisor.fortunica-app.com/login')
+  })
+
+  const fillForm = () => ({
+    email: cy.get('input[name="u"]').type('abc@example.com'),
+    password: cy.get('input[name="p"]').type('asdfghjkl'),
+  })
+
+  it('foobar', () => {
+    const { email, password } = fillForm()
+    console.log(email, password)
+    email.should('have.value', 'abc@example.com')
+    password.should('have.value', 'asdfghjkl')
+  })
 })
